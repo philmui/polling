ac4f28b5065cdb180bd20ca5f4530f799a21fcab
Phil Mui <thephilmui@gmail.com>
2015-12-28 17:26:18 -0800

    Completed voting actions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # polling
+A simple django app showing how to setup polling, views
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.db.models import F
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -16,11 +18,23 @@ def detail(request, question_id):
 				  {'question': question})
 
 def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/results.html',
-				  {'question_id': question_id})
+				  {'question': question})
 
 def vote(request, question_id):
-	return render(request, 'polls/vote.html',
-				  {'question_id': question_id})
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		choice_id = request.POST['choice']
+		selected_choice = question.choice_set.get(pk=choice_id)
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/details.html',
+					  {'question': question,
+					   'error_message': "No choice has been selected yet"})
+	else:
+		selected_choice.vote_tally = F('vote_tally') + 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',
+					  args=(question_id,)))
 
 
