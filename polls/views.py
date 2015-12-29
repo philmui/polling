131dc6986b3bd2a785c6487957d439c961f64e79
Phil Mui <thephilmui@gmail.com>
2015-12-28 17:48:24 -0800

    Using generic views for rendering db data pages

--- polls/views.py
+++ polls/views.py
@@ -2,25 +2,24 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.db.models import F
+from django.views import generic
 
 from .models import Choice, Question
 
-# Create your views here.
-def index(request):
-	latest_question_list = \
-		Question.objects.order_by('-pub_date')[:5]
-	return render(request, 'polls/index.html', 
-				  {'latest_question_list' : latest_question_list})
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/details.html',
-				  {'question': question})
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html',
-				  {'question': question})
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
@@ -28,7 +27,7 @@ def vote(request, question_id):
 		choice_id = request.POST['choice']
 		selected_choice = question.choice_set.get(pk=choice_id)
 	except (KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/details.html',
+		return render(request, 'polls/detail.html',
 					  {'question': question,
 					   'error_message': "No choice has been selected yet"})
 	else:
